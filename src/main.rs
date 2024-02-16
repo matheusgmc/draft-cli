0917337b234c209ae3c3e7193024c77fa39d441f
Matheus Geraldo <matheus.geraldo1@outlook.com>
2024-02-16 16:34:05 -0400

    chore: change version

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ pub mod utils;
 fn main() {
     let matches = Command::new("draft-cli")
         .about("Create projects")
-        .version("0.1.0")
+        .version("0.2.0")
         .subcommand_required(true)
         .arg_required_else_help(true)
         .author("matheusgmc")
